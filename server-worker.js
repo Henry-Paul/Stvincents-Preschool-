d9c3782ada337c80e1051fa79363b7e0e9006ea2
Henry-Paul <164559529+Henry-Paul@users.noreply.github.com>
2025-11-20 22:00:04 +0530

    Update server-worker.js

--- server-worker.js
+++ server-worker.js
@@ -1,48 +1,74 @@
-const CACHE = 'stvincent-v1';
-const OFFLINE_URL = '/St-Vincent-s-Preschool/index.html';
+const CACHE_NAME = 'st-vincents-v1.0.0';
+const RUNTIME_CACHE = 'st-vincents-runtime';
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => {
-      return cache.addAll([
-        OFFLINE_URL,
-        '/St-Vincent-s-Preschool/styles.css',
-        '/St-Vincent-s-Preschool/manifest.json'
-      ]).catch(() => {
-        console.log('Some files not available for caching during install');
-      });
-    })
-  );
-  self.skipWaiting();
-});
+const STATIC_ASSETS = [
+    '/',
+    '/index.html',
+    '/programs.html',
+    '/gallery.html',
+    '/blogs.html',
+    '/faq.html',
+    '/contact.html',
+    '/css/styles.css',
+    '/js/main.js',
+    '/manifest.json'
+];
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  if(event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request)
-        .catch(() => caches.match(OFFLINE_URL))
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(STATIC_ASSETS))
+            .then(() => self.skipWaiting())
     );
-    return;
-  }
-
-  event.respondWith(
-    caches.match(event.request)
-      .then(res => res || fetch(event.request))
-      .catch(() => new Response('Offline - Resource not available'))
-  );
 });
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys()
+            .then((cacheNames) => {
+                return Promise.all(
+                    cacheNames
+                        .filter((name) => name !== CACHE_NAME && name !== RUNTIME_CACHE)
+                        .map((name) => caches.delete(name))
+                );
+            })
+            .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    const { request } = event;
+    const url = new URL(request.url);
+    
+    if (url.origin !== location.origin) {
+        return;
+    }
+    
+    event.respondWith(cacheFirst(request));
+});
+
+async function cacheFirst(request) {
+    const cache = await caches.open(CACHE_NAME);
+    const cached = await cache.match(request);
+    
+    if (cached) {
+        return cached;
+    }
+    
+    try {
+        const response = await fetch(request);
+        
+        if (response.ok) {
+            const runtimeCache = await caches.open(RUNTIME_CACHE);
+            runtimeCache.put(request, response.clone());
+        }
+        
+        return response;
+    } catch (error) {
+        const offlinePage = await cache.match('/index.html');
+        if (offlinePage) {
+            return offlinePage;
+        }
+        throw error;
+    }
+}
